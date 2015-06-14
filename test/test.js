5377fa0261d85aa80d184c7db01f20f0379a2a95
Will <yh@fir.im>
2015-06-14 22:57:38 +0800

    add interface

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var Yunpian = require('./lib/yunpian')
 
 
-var tpl1 = '【fir.im】活动在海淀大街34号海置创投6层，地铁中关村站E口出向西走，可参考：http://j.map.baidu.com/U5kB3';
-var tpl2 = '【fir.im】很高兴你可以来陪 fir.im 一起过生日，今天下午13：40开始签到，聚会地点是：海淀大街34号海置创投6层 ，下午见~~';
+var tpl1 = '';
+var tpl2 = '';
 SMS = new Yunpian('****************************')
 
 
