150557bcb96248db413d0a165562dc4fbb70cea0
Will <yh@fir.im>
2015-09-24 21:39:33 +0800

    修改测试

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-var Yunpian = require('./lib/yunpian')
+var Yunpian = require('../lib/yunpian')
 
 
-var tpl1 = '';
-var tpl2 = '';
-SMS = new Yunpian('****************************')
+var tpl = '';
+var arr_str = ""
+SMS = new Yunpian('')
 
 
 hash = {
-  mobile: '13581648716',
-  text: tpl2
+  mobile: arr_str,
+  text: tpl
 }
 
-SMS.sendSms(hash,function(err,body){
+SMS.sendSms(hash, function(err,body){
   console.log(err,body)
 })
 
